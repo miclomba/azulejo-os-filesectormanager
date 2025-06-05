db4d3de35aa62068f18eaafc385828d5da49b94e
mclombi <miclomba@gmail.com>
2025-06-05 01:22:24 -0700

    [] Add validation against output baseline

--- qaStart.sh
+++ qaStart.sh
@@ -4,13 +4,20 @@
 # *	Author: Michael Lombardi
 # **********************************/
 
+echo ">>> Initializing FS Data..."
 make touch_data
+echo ">>> Cleaning the Project..."
 make clean
+echo ">>> Building..."
 make
 
 #Use input file
+echo ">>> Running the Driver..."
 ./test/fsm 1 > test/qaOutput.txt
 
 #Use stub input file
 #./test/fsm 1 1 > test/qaOutput.txt
+
+echo ">>> Comparing Outputs..."
+diff test/qaOutput.txt test/qaOutputBaseline.txt
 
