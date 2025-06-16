a1820bb9ccdae689234cdd18a68f0b29026c8aa0
mclombi <miclomba@gmail.com>
2025-06-16 10:33:51 -0700

    [] Refactor inode.c to remove loops

--- src/inode.c
+++ src/inode.c
@@ -40,101 +40,37 @@ void inode_make(unsigned int _count, FILE *_fileStream, unsigned int _diskOffset
 }
 
 void inode_read(Inode *_inode, unsigned int _inodeNum, FILE *_fileStream) {
-    // iterators for moving through arrays
-    int j, k;
-    // store the temporary value of the return value from fread
-    // int sampleCount;
-    // will need to determine an offset from the first iNode to
-    // the iNode we want to read from
-    int offset;
     // array buffer to hold values from the iNode buffer passed into function
-    unsigned int buffer[20];
+    Inode buffer;
+    // will need to determine an offset from the first iNode to the iNode we want to read from
     // generate the offset based on the size of the block, size of the iNode
     // and the iNode number in which we will read from
-    offset = (2 * BLOCK_SIZE) + (_inodeNum * INODE_SIZE);
+    int offset = (2 * BLOCK_SIZE) + (_inodeNum * INODE_SIZE);
     // move to the offset location of the filestream
     fseek(_fileStream, offset, SEEK_SET);
     // read the offset location in the filestream
-    size_t res = fread(buffer, sizeof(unsigned int), 20, _fileStream);
+    size_t res = fread(&buffer, sizeof(Inode), 1, _fileStream);
     if (res == 0) {
         printf("Error reading inode %d from file stream.\n", _inodeNum);
         return;
     }
     // move to the start of the filestream
     fseek(_fileStream, 0, SEEK_SET);
-    // store buffer for fileType
-    _inode->fileType = buffer[0];
-    // store buffer for fileSize
-    _inode->fileSize = buffer[1];
-    // store buffer for permissions
-    _inode->permissions = buffer[2];
-    // store buffer for linkCount
-    _inode->linkCount = buffer[3];
-    // store buffer for dataBlocks
-    _inode->dataBlocks = buffer[4];
-    // store buffer for tModified
-    _inode->owner = buffer[5];
-    // store buffer for status
-    _inode->status = buffer[6];
-    // set the starting point to retrieve directPtr values from buffer
-    k = 0;
-    // retrieve directPtrs from the read buffer
-    for (j = 7; j < 17; j++) {
-        // iNode directPtr value stores value from buffer
-        _inode->directPtr[k] = buffer[j];
-        // move to next directPtr
-        k++;
-    }  // end for (j = 7; j < 17; j++)
-    // get the indirect pointers
-    _inode->sIndirect = buffer[17];
-    _inode->dIndirect = buffer[18];
-    _inode->tIndirect = buffer[19];
+    // store buffer for:
+    // fileType, fileSize, permissions, linkCount, dataBlocks, owner,
+    // status, directPtr[10], sIndirect, dIndirect, tIndirect
+    memcpy(_inode, &buffer, sizeof(Inode));
 }
 
 void inode_write(Inode *_inode, unsigned int _inodeNum, FILE *_fileStream) {
-    // iterators for moving through arrays
-    int j, k;
-    // store the temporary value returned from fwrite
-    // int sampleCount;
-    // will need to determine an offset from the first iNode to
-    // the iNode we want to write to
-    int offset;
-    // array buffer to hold values from the iNode buffer passed into function
-    unsigned int buffer[20];
+    // will need to determine an offset from the first iNode to the iNode we want to write to
     // generate the offset based on the size of the block, size of the iNode
     // and the iNode number in which we will write to
-    offset = (2 * BLOCK_SIZE) + (_inodeNum * INODE_SIZE);
-    // store buffer for fileType
-    buffer[0] = _inode->fileType;
-    // store buffer for fileSize
-    buffer[1] = _inode->fileSize;
-    // store buffer for permissions
-    buffer[2] = _inode->permissions;
-    // store buffer for linkCount
-    buffer[3] = _inode->linkCount;
-    // store buffer for dataBlocks
-    buffer[4] = _inode->dataBlocks;
-    // store buffer for tModified
-    buffer[5] = _inode->owner;
-    // store buffer for status
-    buffer[6] = _inode->status;
-    // set the starting point to retrieve directPtr values from buffer
-    k = 0;
-    // retrieve directPtr values from iNod buffer
-    for (j = 7; j < 17; j++) {
-        // buffer value for directPtr will have value from iNode buffer
-        buffer[j] = _inode->directPtr[k];
-        // move to next directPtr in iNode buffer
-        k++;
-    }  // end for (j = 7; j < 17; j++)
-    // get the indirect pointers
-    buffer[17] = _inode->sIndirect;
-    buffer[18] = _inode->dIndirect;
-    buffer[19] = _inode->tIndirect;
+    int offset = (2 * BLOCK_SIZE) + (_inodeNum * INODE_SIZE);
     // move to the offset location of the filestream
     fseek(_fileStream, offset, SEEK_SET);
     // write to the offset location in the filestream
-    fwrite(buffer, sizeof(unsigned int), 20, _fileStream);
+    fwrite(_inode, sizeof(Inode), 1, _fileStream);
     // move to the start of the filestream
     fseek(_fileStream, 0, SEEK_SET);
 }
